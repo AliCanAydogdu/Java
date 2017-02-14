ce8c9d05815e10740ae0ab2549e7ec9a1b76e9a6
Ali Can Aydogdu <alicantx@gmail.com>
2017-02-14 17:51:20 -0600

    Initial Commit

---
+++ OlympicRings.java
@@ -0,0 +1,32 @@
+//********************************************************************
+//  OlympicRings.java       Author: Lewis/Loftus
+//
+//  Solution to Programming Project 2.18
+//********************************************************************
+
+import javax.swing.JApplet;
+import java.awt.*;
+
+public class OlympicRings extends JApplet
+{
+   //-----------------------------------------------------------------
+   //  Draws the olympic logo.
+   //-----------------------------------------------------------------
+   public void paint (Graphics page)
+   {
+      final int DIAMETER = 50;
+
+      setBackground (Color.lightGray);
+
+      page.setColor (Color.blue);
+      page.drawOval (30, 40, DIAMETER, DIAMETER);
+      page.setColor (Color.yellow);
+      page.drawOval (60, 70, DIAMETER, DIAMETER);
+      page.setColor (Color.black);
+      page.drawOval (90, 40, DIAMETER, DIAMETER);
+      page.setColor (Color.green);
+      page.drawOval (120, 70, DIAMETER, DIAMETER);
+      page.setColor (Color.red);
+      page.drawOval (150, 40, DIAMETER, DIAMETER);
+   }
+}
