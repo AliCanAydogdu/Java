bf88e09302be3be659efb60dd7388333b41d5ca3
Ali Can Aydogdu <alicantx@gmail.com>
2017-05-05 20:23:28 -0500

    Initial Commit

---
+++ PayCheck.java
@@ -0,0 +1,37 @@
+
+import javax.swing.JOptionPane;
+
+class PayCheck
+{
+   //-----------------------------------------------------------------
+   //  Finds and displays the gross pay for an hourly worker
+   //  Uses multiple dialog boxes for user interaction.
+   //-----------------------------------------------------------------
+   public static void main (String[] args)
+   {
+       String hoursStr, rateStr, result;
+       double hours, rate, pay;
+       int again;
+
+      do
+      {
+         hoursStr = JOptionPane.showInputDialog ("Enter hours worked: ");
+         hours = Double.parseDouble(hoursStr);
+
+         rateStr = JOptionPane.showInputDialog ("Enter hourly rate: ");
+         rate = Double.parseDouble(rateStr);
+
+	 if (hours > 40)
+	     pay = 40 * rate + (hours-40) * rate * 1.5;
+	 else
+	     pay = hours * rate;
+
+         result = "Gross pay is " + pay;
+
+         JOptionPane.showMessageDialog (null, result);
+
+         again = JOptionPane.showConfirmDialog (null, "Do Another?");
+      }
+      while (again == JOptionPane.YES_OPTION);
+   }
+}
