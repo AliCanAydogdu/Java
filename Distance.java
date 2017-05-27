1d8828e13ff2f8ce7814314c2d96c1ba9d884947
Ali Can Aydogdu <alicantx@gmail.com>
2017-05-27 15:20:46 -0500

    Update Distance.java

--- Distance.java
+++ Distance.java
@@ -4,18 +4,22 @@ public class Distance
 {
     public static void main (String[] args)
     {
-	double x1, y1, x2, y2; // coordinates of two points
-	double distance;       // distance between the points
+	// coordinates of two points
+	double x1, y1, x2, y2; 
+	// distance between the points
+	double distance;       
 
 	Scanner scan = new Scanner (System.in);
 
 	// Read in the two points
 	System.out.print ("Enter the coordinates of the first point " +
                "(put a space between them): ");
+	    
 	x1 = scan.nextDouble();
 	y1 = scan.nextDouble();
 
 	System.out.print ("Enter the coordinates of the second point: ");
+	    
 	x2 = scan.nextDouble();
 	y2 = scan.nextDouble();
 
